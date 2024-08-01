2a4184866875fac65bb9f135ddca5c279ecea181
jdalton <john.david.dalton@gmail.com>
2024-08-01 09:59:14 -0400

    chore: prettier pass

--- src/package-url.js
+++ src/package-url.js
@@ -33,7 +33,8 @@ const KnownQualifierNames = Object.freeze({
 
 // This regexp is valid as of 2024-08-01.
 // https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
-const regexSemverNumberedGroups = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/
+const regexSemverNumberedGroups =
+    /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/
 
 const normalize = {
     alpm(purl) {
